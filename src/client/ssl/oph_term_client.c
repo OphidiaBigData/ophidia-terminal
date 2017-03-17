4cf9f124f14a74634131520e107a18daa7a6c04e
eldoo <eldoo@users.noreply.github.com>
2017-03-17 10:39:47 +0100

    Fix code style

--- src/client/ssl/oph_term_client.c
+++ src/client/ssl/oph_term_client.c
@@ -517,22 +517,22 @@ int oph_term_client(char *cmd_line, char *command, char **newsession, char *user
 #ifdef WITH_OPENSSL
 
 #if defined(WIN32)
-# define MUTEX_TYPE     HANDLE
-# define MUTEX_SETUP(x)     (x) = CreateMutex(NULL, FALSE, NULL)
-# define MUTEX_CLEANUP(x)   CloseHandle(x)
-# define MUTEX_LOCK(x)      WaitForSingleObject((x), INFINITE)
-# define MUTEX_UNLOCK(x)    ReleaseMutex(x)
-# define THREAD_ID      GetCurrentThreadId()
+#define MUTEX_TYPE     HANDLE
+#define MUTEX_SETUP(x)     (x) = CreateMutex(NULL, FALSE, NULL)
+#define MUTEX_CLEANUP(x)   CloseHandle(x)
+#define MUTEX_LOCK(x)      WaitForSingleObject((x), INFINITE)
+#define MUTEX_UNLOCK(x)    ReleaseMutex(x)
+#define THREAD_ID      GetCurrentThreadId()
 #elif defined(_POSIX_THREADS) || defined(_SC_THREADS)
-# define MUTEX_TYPE     pthread_mutex_t
-# define MUTEX_SETUP(x)     pthread_mutex_init(&(x), NULL)
-# define MUTEX_CLEANUP(x)   pthread_mutex_destroy(&(x))
-# define MUTEX_LOCK(x)      pthread_mutex_lock(&(x))
-# define MUTEX_UNLOCK(x)    pthread_mutex_unlock(&(x))
-# define THREAD_ID      pthread_self()
+#define MUTEX_TYPE     pthread_mutex_t
+#define MUTEX_SETUP(x)     pthread_mutex_init(&(x), NULL)
+#define MUTEX_CLEANUP(x)   pthread_mutex_destroy(&(x))
+#define MUTEX_LOCK(x)      pthread_mutex_lock(&(x))
+#define MUTEX_UNLOCK(x)    pthread_mutex_unlock(&(x))
+#define THREAD_ID      pthread_self()
 #else
-# error "You must define mutex operations appropriate for your platform"
-# error "See OpenSSL /threads/th-lock.c on how to implement mutex on your platform"
+#error "You must define mutex operations appropriate for your platform"
+#error "See OpenSSL /threads/th-lock.c on how to implement mutex on your platform"
 #endif
 
 struct CRYPTO_dynlock_value {
