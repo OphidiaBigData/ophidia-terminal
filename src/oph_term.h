aa0a1310d3d060527c012168eed38ac961cfca70
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2021-10-12 16:45:40 +0200

    Add alias to execute a workflow from a checkpoint

--- src/oph_term.h
+++ src/oph_term.h
@@ -162,6 +162,7 @@ extern int oph_term_error_cur;
 #define OPH_TERM_DEFAULT_ALIAS_KEY_27 	"rmd"
 #define OPH_TERM_DEFAULT_ALIAS_KEY_28 	"rmr"
 #define OPH_TERM_DEFAULT_ALIAS_KEY_29 	"pav_worker"
+#define OPH_TERM_DEFAULT_ALIAS_KEY_30 	"repeat"
 
 #define OPH_TERM_DEFAULT_ALIAS_VAL_1 	"oph_list path=$1;"
 #define OPH_TERM_DEFAULT_ALIAS_VAL_2 	"oph_folder command=cd;path=$1;"
@@ -192,8 +193,9 @@ extern int oph_term_error_cur;
 #define OPH_TERM_DEFAULT_ALIAS_VAL_27 	"oph_fs command=rm;dpath=$1;"
 #define OPH_TERM_DEFAULT_ALIAS_VAL_28 	"oph_deletecontainer container=$1;force=yes;"
 #define OPH_TERM_DEFAULT_ALIAS_VAL_29 	"oph_pav_worker action=$1;number=$2;"
+#define OPH_TERM_DEFAULT_ALIAS_VAL_30 	"oph_resume id=$1;document_type=request;execute=$2;"
 
-#define OPH_TERM_DEFAULT_ALIAS_NUM 29
+#define OPH_TERM_DEFAULT_ALIAS_NUM 30
 
 extern const char *pre_defined_aliases_keys[OPH_TERM_DEFAULT_ALIAS_NUM];
 extern const char *pre_defined_aliases_values[OPH_TERM_DEFAULT_ALIAS_NUM];
